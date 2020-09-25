9313f62e72812a42308f1e3110d0cb449c334161
tyupoid <thiago_tyupoid@hotmail.com>
2020-09-25 16:04:03 -0300

    CLASS 1 CODE3R

---
+++ src/main/java/br/com/course/coursesb/CourseSbApplication.java
@@ -0,0 +1,13 @@
+package br.com.course.coursesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/course/coursesb/CourseSbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.course.coursesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
