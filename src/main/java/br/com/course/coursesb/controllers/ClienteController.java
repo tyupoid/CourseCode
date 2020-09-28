a24d10f0415dd5a721a750d151f17354cb764edc
tyupoid <thiago_tyupoid@hotmail.com>
2020-09-28 14:34:45 -0300

    passing argument

--- src/main/java/br/com/course/coursesb/controllers/ClienteController.java
+++ src/main/java/br/com/course/coursesb/controllers/ClienteController.java
@@ -1,6 +1,8 @@
 package br.com.course.coursesb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +15,10 @@ public class ClienteController {
 	@GetMapping("/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(28,"pedro","123.442,897-00");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id,"maria","987.654.321-00");
 	}
 }
