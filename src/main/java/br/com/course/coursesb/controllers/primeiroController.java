d86b1bad25ab9d0a0c734da847dff5852b42f0c7
tyupoid <thiago_tyupoid@hotmail.com>
2020-09-28 12:30:01 -0300

    First WebService Class Leonardo Leitao

--- src/main/java/br/com/course/coursesb/controllers/primeiroController.java
+++ src/main/java/br/com/course/coursesb/controllers/primeiroController.java
@@ -1,9 +1,14 @@
 package br.com.course.coursesb.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class primeiroController {
+
+	@GetMapping(path = { "/ola", "/saudacao" })
 	public String ola() {
 		return "ola Spring boot!";
 	}
+
 }
